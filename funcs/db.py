c1c7dfc1aef93b56c5892af25f23ef2b37699c81
Andrey <128032974+TINKERER11@users.noreply.github.com>
2024-10-18 19:18:05 +0700

    realise

--- funcs/db.py
+++ funcs/db.py
@@ -1,13 +1,14 @@
+import os
 import psycopg2
 
 
 def connection_to_db():
     conn = psycopg2.connect(
-        host="localhost",
-        port=5432,
-        user="postgres",
-        password="yfhmzy03",
-        dbname="oprosy"
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        dbname=os.getenv("DB_NAME")
     )
     return conn
 
